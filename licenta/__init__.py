af79238495f863e9e2ca6186142040388791c9fd
alexrot15 <alexrotaru1595@gmail.com>
2021-03-21 12:56:04 +0200

    update db

--- licenta/__init__.py
+++ licenta/__init__.py
@@ -8,7 +8,8 @@ from flask_mail import Mail
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///baza_de_date.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", "sqlite:///baza_de_date.db")
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
